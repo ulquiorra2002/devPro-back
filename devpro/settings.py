9fc8e5326b1c541e04965029685f6cd077bf6099
Ang98 <angelcayatopa98@gmail.com>
2021-03-23 18:04:50 -0500

    first deploy

--- devpro/settings.py
+++ devpro/settings.py
@@ -96,17 +96,6 @@ DATABASES = {
         'HOST':'bev9ycfrbmrovoadpooc-mysql.services.clever-cloud.com'
     }
 }
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bev9ycfrbmrovoadpooc',
-        'USER':'uyikd1tl92incu9b',
-        'PASSWORD':'t9kMr8fv2NG1EfroSfPC',
-        'HOST':'bev9ycfrbmrovoadpooc-mysql.services.clever-cloud.com'
-    }
-}
-"""
 
 
 # Password validation
